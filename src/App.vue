bb53dac2ed39a421f6c5b979a1248971487d2021
subhendu <bhattasubhendu@gmail.com>
2021-02-11 09:48:02 +0545

    app with storage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <h1>{{appTitle}}</h1>
+
+    <h3>
+      List of todo 
+    </h3>
+
+      <input type="text" v-model="newTodoTitle" placeholder="enter your task" @keydown.enter="addTodo">
+
+      <button v-if="newTodoTitle" @click="addTodo" >Add</button>
+    <ul>
+      <li v-for="(todo,index) in todos" :key="index">
+
+        <input v-if="todo.editMode" type="text" v-model="todo.name" @keydown.enter="updateTodo(todo)">
+        <span v-else :class="{completed: todo.done}">{{todo.name}} </span> 
+        
+        <div>
+        <button @click="toggleDone(todo)">{{todo.done ? 'mark undone': 'mark done'}}</button>
+        <button @click="removeTodo(todo)">del</button>
+        <button @click="editTodo(todo)">{{todo.editMode? 'cancel edit': 'edit'}}</button>
+        </div>
+        
+
+      </li>
+     
+    </ul>
+
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const STORAGE_KEY = 'nice-todo';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      idCounter: 0,
+      newTodoTitle: '',
+      appTitle: "Nice Todo app",
+      todos: JSON.parse(localStorage.getItem(STORAGE_KEY)) || [] 
+    }
+  },
+
+  watch:{
+
+    todos : {
+      handler(newTodos) {
+          localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+      },
+
+      deep: true
+    }
+ 
+  
+  },
+
+  methods:{
+    addTodo(){
+      this.todos.push({
+        id: this.idCounter,
+        name: this.newTodoTitle,
+        done: false,
+        editMode: false
+      })
+      this.newTodoTitle = ''
+      this.idCounter++;
+
+    },
+
+    toggleDone(todo){
+      todo.done = !todo.done;
+    },
+    
+    removeTodo(todo){
+      this.todos = this.todos.filter(item=> item.id != todo.id)
+    },
+    editTodo(todo) {
+
+      todo.editMode = ! todo.editMode;
+
+    }
+    ,
+    updateTodo(todo) {
+        todo.editMode = false;
+    }
+
+  },
+
+
+  created(){
+    if(this.todos.length) {
+      this.idCounter = this.todos[this.todos.length-1].id + 1
+    }
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.completed{
+  text-decoration: line-through;
 }
+
 </style>
